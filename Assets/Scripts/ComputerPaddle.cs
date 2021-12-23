68ce45c6dd49ff2769ab7b4ab7e01779601ea812
DratBoi <96489024+DratBoi@users.noreply.github.com>
2021-12-23 21:43:11 +0530

    Add files via upload

---
+++ Assets/Scripts/ComputerPaddle.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class ComputerPaddle : Paddle
+{
+    public Rigidbody2D ball;
+    
+    private void FixedUpdate()
+    {
+        if (this.ball.velocity.x > 0.0f)
+        {
+            if (this.ball.position.y > this.transform.position.y) {
+                _rigidbody.AddForce(Vector2.up * this.Speed);
+            } else if (this.ball.position.y < this.transform.position.y) {
+                _rigidbody.AddForce(Vector2.down * this.Speed);
+            }
+        }
+        else
+        {
+            if (this.transform.position.y > 0.0f) {
+                _rigidbody.AddForce(Vector2.down * this.Speed);
+            } else if (this.transform.position.y < 0.0f) {
+                _rigidbody.AddForce(Vector2.up * this.Speed);
+            }
+        }
+    }
+}
